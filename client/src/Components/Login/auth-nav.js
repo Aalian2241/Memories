78c2da2570d4371157e2dfc082996dcb26a8bb4a
Aalian2241 <aalian.2241@gmail.com>
2022-08-07 06:21:07 +0500

    no use auth0 authentificaiton in react

---
+++ client/src/Components/Login/auth-nav.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import AuthenticationButton from './authenticationButton';
+
+const AuthNav = () => (
+  <div className="navbar-nav ml-auto">
+    <AuthenticationButton />
+  </div>
+);
+
+export default AuthNav;
